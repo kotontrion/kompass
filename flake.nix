03362664e6e26ef896abe77b1c51e9b7ba688436
kotontrion <kotontrion@tutanota.de>
2025-04-14 10:07:51 +0200

    refactor: reorganize project structure
    * Move kompass and libkompass out of subprojects directory
    * Create unified build system with meson options for targets
    * Simplify project configuration and dependencies
    * Update resource names to avoid conflicts
    * Fix build dependency paths for new layout

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
   inputs = {
+    
     astal = {
       type = "github";
       owner = "Aylur";
@@ -15,107 +16,118 @@
     };
   };
 
-  outputs = {
-    self,
-    systems,
-    nixpkgs,
-    astal,
-    ...
-  }: let
+  outputs = { self, nixpkgs, astal, systems, ... }: let
+    
     perSystem = attrs:
       nixpkgs.lib.genAttrs (import systems) (system:
         attrs (import nixpkgs {inherit system;}));
-
     inherit (nixpkgs.lib) attrValues elemAt getExe head match readFile splitString;
     getMesonValue = val: file:
       head (match "^([^']*).*" (elemAt (splitString val (readFile file)) 1));
   in {
     packages = perSystem (pkgs: {
-      libkompass = pkgs.callPackage (
-        {
-          stdenv,
-          lib,
-          ...
-        }: let
-          pname = "libkompass";
-          version = getMesonValue "version: '" "${self}/subprojects/libkompass/meson.build";
-
-          apiVersion = getMesonValue "api_version = '" "${self}/subprojects/libkompass/src/meson.build";
-        in
-          stdenv.mkDerivation {
-            inherit pname version;
-
-            src = self;
-            sourceRoot = "source/subprojects/libkompass";
-
-            postPatch = ''
-              substituteInPlace ./src/screenrecord.vala \
-                  --replace-fail "slurp" "${getExe pkgs.slurp}" \
-                  --replace-fail "bash -c 'wf-recorder" "${getExe pkgs.bash} -c '${getExe pkgs.wf-recorder}"
-            '';
-
-            nativeBuildInputs = attrValues {
-              # Build Dependencies as declared in src/meson.build
-              inherit
-                (pkgs)
-                libadwaita # libadwaita-1
-                wrapGAppsHook4 # gio-2.0
-                libportal # libportal
-                ;
-              inherit
-                (astal.packages.${pkgs.system})
-                apps # astal-apps-0.1
-                bluetooth # astal-bluetooth-0.1
-                io # astal-io-0.1
-                cava # astal-cava-0.1
-                mpris # astal-mpris-0.1
-                notifd # astal-notifd-0.1
-                tray # astal-tray-0.1
-                river # astal-river-0.1
-                wireplumber # astal-wireplumber-0.1
-                ;
-
-              # Build Dependencies as declared in data/ui/meson.build
-              inherit
-                (pkgs)
-                blueprint-compiler # blueprint-compiler
-                ;
-
-              # Build Dependencies as declared in data/scss/meson.build
-              inherit
-                (pkgs)
-                dart-sass # sass
-                ;
-
-              # Build tools
-              inherit
-                (pkgs)
-                gawk # awk
-                gobject-introspection # g-ir-compiler
-                libxml2 # xmllint
-                meson
-                ninja
-                pkg-config
-                vala
-                ;
-            };
-
-            passthru.girName = "Kompass-0.1";
-
-            meta = {
-              license = lib.licenses.lgpl3Only;
-            };
-          }
-      ) {};
-
-      docs = pkgs.callPackage ({
-        stdenv,
-        lib,
-        gobject-introspection,
-        gi-docgen,
-        ...
-      }: let
+      libkompass = pkgs.stdenv.mkDerivation {
         pname = "libkompass";
+        version = getMesonValue "version: '" "${self}/meson.build";
+        apiVersion = getMesonValue "api_version = '" "${self}/libkompass/meson.build";
+        src = ./.;
+
+        mesonFlags = [
+          "-Dbuild_target=libkompass"
+        ];
+
+        postPatch = ''
+          substituteInPlace ./libkompass/src/screenrecord.vala \
+              --replace-fail "slurp" "${getExe pkgs.slurp}" \
+              --replace-fail "bash -c 'wf-recorder" "${getExe pkgs.bash} -c '${getExe pkgs.wf-recorder}"
+        '';
+
+        nativeBuildInputs = with pkgs; [
+          meson
+          ninja
+          pkg-config
+          vala
+          gobject-introspection
+          blueprint-compiler
+          dart-sass
+          gawk
+          libxml2
+        ];
+
+        buildInputs = attrValues {
+          inherit (astal.packages.${pkgs.system})
+            io
+            apps
+            battery
+            bluetooth
+            cava
+            mpris
+            network
+            notifd
+            river
+            tray
+            wireplumber
+            ;
+          inherit (pkgs)
+            libadwaita
+            libportal
+            gtk4
+            ;
+        };
+
+        passthru.girName = "Kompass-${self.packages.${pkgs.system}.libkompass.apiVersion}";
+      };
+      kompass = pkgs.stdenv.mkDerivation {
+        pname = "kompass";
+        version = getMesonValue "version: '" "${self}/meson.build";
+        src = ./.;
+
+        mesonFlags = [
+          "-Dbuild_target=kompass"
+        ];
+
+        postPatch = ''
+          substituteInPlace ./libkompass/src/screenrecord.vala \
+              --replace-fail "slurp" "${getExe pkgs.slurp}" \
+              --replace-fail "bash -c 'wf-recorder" "${getExe pkgs.bash} -c '${getExe pkgs.wf-recorder}"
+        '';
+
+        nativeBuildInputs = with pkgs; [
+          meson
+          ninja
+          pkg-config
+          vala
+          gobject-introspection
+          blueprint-compiler
+          dart-sass
+          gawk
+        ];
+
+        buildInputs = attrValues {
+          inherit (astal.packages.${pkgs.system})
+            io
+            astal4
+            apps
+            battery
+            bluetooth
+            cava
+            mpris
+            network
+            notifd
+            river
+            tray
+            wireplumber
+            ;
+          inherit (pkgs)
+            libadwaita
+            libportal
+            gtk4
+            ;
+        };
+      };
+
+      docs = let
+        pname = "libkompass-docs";
         libkompass = self.packages.${pkgs.system}.libkompass;
         version = libkompass.version;
         girFile = "${libkompass}/share/gir-1.0/${libkompass.passthru.girName}.gir";
@@ -155,19 +167,17 @@
           ]}
         '';
       in
-        stdenv.mkDerivation {
+        pkgs.stdenv.mkDerivation {
           inherit pname version;
           src = self;
-          sourceRoot = "source/subprojects/libkompass";
-          nativeBuildInputs = [
-            gobject-introspection
-            gi-docgen
-          ];
-          buildInputs = attrValues {
+          sourceRoot = "source/libkompass";
+          nativeBuildInputs = attrValues {
               # Build Dependencies as declared in src/meson.build
               inherit
                 (pkgs)
                 libadwaita # libadwaita-1
+                gobject-introspection
+                gi-docgen
                 ;
               inherit
                 (astal.packages.${pkgs.system})
@@ -192,90 +202,17 @@
           '';
           meta = {
             description = "Documentation for libkompass";
-            license = lib.licenses.lgpl3Only;
+            license = pkgs.lib.licenses.lgpl3Only;
           };
-        }) {};
-
-      kompass = pkgs.callPackage (
-        {
-          stdenv,
-          lib,
-          ...
-        }: let
-          pname = "kompass";
-          version = getMesonValue "version: '" "${self}/subprojects/kompass/meson.build";
-        in
-          stdenv.mkDerivation {
-            inherit pname version;
-
-            src = self;
-            sourceRoot = "source/subprojects/kompass";
-
-            nativeBuildInputs = attrValues {
-              # Build Dependencies as declared in src/meson.build
-              inherit
-                (self.packages.${pkgs.system})
-                libkompass # kompass-0.1
-                ;
-              inherit
-                (pkgs)
-                wrapGAppsHook4 # gio-2.0
-                gtk4 # gtk4
-                gtk4-layer-shell # gtk4-layer-shell-0
-                libadwaita # libadwaita-1
-                networkmanager # libnm
-                ;
-              inherit
-                (astal.packages.${pkgs.system})
-                astal4 # astal-4-4.0
-                io # astal-io-0.1
-                mpris # astal-mpris-0.1
-                river # astal-river-0.1
-                battery # astal-battery-0.1
-                bluetooth # astal-bluetooth-0.1
-                network # astal-network-0.1
-                notifd # astal-notifd-0.1
-                apps # astal-apps-0.1
-                wireplumber # astal-wireplumber-0.1
-                ;
-
-              # Build Dependencies as declared in data/ui/meson.build
-              inherit
-                (pkgs)
-                blueprint-compiler # blueprint-compiler
-                ;
-
-              # Build Dependencies as declared in data/scss/meson.build
-              inherit
-                (pkgs)
-                dart-sass # sass
-                ;
-
-              # Build tools
-              inherit
-                (pkgs)
-                gawk # awk
-                libxml2 # xmllint
-                gobject-introspection # for typelibs
-                meson
-                ninja
-                pkg-config
-                vala
-                ;
-            };
-
-            meta = {
-              license = lib.licenses.gpl3Only;
-            };
-          }
-      ) {};
+        };
 
       default = self.packages.${pkgs.system}.kompass;
     });
 
     devShells = perSystem (pkgs: {
       default = pkgs.mkShell {
-        nativeBuildInputs = with self.packages.${pkgs.system}; (libkompass.nativeBuildInputs ++ kompass.nativeBuildInputs);
+        nativeBuildInputs = with self.packages.${pkgs.system}; (kompass.nativeBuildInputs ++ libkompass.nativeBuildInputs);
+        buildInputs = with self.packages.${pkgs.system}; (kompass.buildInputs ++ libkompass.buildInputs);
         inputsFrom = attrValues {
           inherit
             (self.packages.${pkgs.system})
@@ -285,7 +222,5 @@
         };
       };
     });
-
-    formatter = perSystem (pkgs: pkgs.alejandra);
   };
 }
