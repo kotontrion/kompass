6dc3eff298b5a419d683abd25779febba1e3ec1b
[Woodpecker CI] kotontrion <kotontrion@noreply.codeberg.org>
2026-04-06 11:57:40 +0000

    [CI SKIP] Deploy docs from https://codeberg.org/kotontrion/kompass/commit/e78b4c54de4280e2cb015eebd987d8a18dd2a618

---
+++ urlmap.js
@@ -0,0 +1,19 @@
+baseURLs = [
+    ["GLib", "https://docs.gtk.org/glib/"],
+    ["GObject", "https://docs.gtk.org/gobject/"],
+    ["Gio", "https://docs.gtk.org/gio/"],
+    ["Gdk", "https://docs.gtk.org/gdk4/"],
+    ["Gtk", "https://docs.gtk.org/gtk4/"],
+    ["GdkPixbuf", "https://docs.gtk.org/gdk-pixbuf/"],
+    ["Adw", "https://gnome.pages.gitlab.gnome.org/libadwaita/doc/1-latest/"],
+    ["AstalIO", "https://aylur.github.io/libastal/io/"],
+    ["AstalNotifd", "https://aylur.github.io/libastal/notifd/"],
+    ["AstalBluetooth", "https://aylur.github.io/libastal/bluetooth/"],
+    ["AstalCava", "https://aylur.github.io/libastal/cava/"],
+    ["AstalMpris", "https://aylur.github.io/libastal/mpris/"],
+    ["AstalRiver", "https://aylur.github.io/libastal/river/"],
+    ["AstalWp", "https://aylur.github.io/libastal/wireplumber/"],
+    ["AstalApps", "https://aylur.github.io/libastal/apps/"],
+    ["AstalNetwork", "https://aylur.github.io/libastal/network/"],
+    ["AstalTray", "https://aylur.github.io/libastal/tray/"]
+]
